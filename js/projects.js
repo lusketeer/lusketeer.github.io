54f8deabe3cd367329d9f329daa613f8d39b9e2b
Luke <luke8520@gmail.com>
2014-06-12 21:23:24 -0700

    Switching between server and local

--- js/projects.js
+++ js/projects.js
@@ -10,7 +10,28 @@
 			console.log("Data fail to load");
 			}		
 		);
-		// this.projects = projects;
+		// this.projects = [
+		// 	{	"heading": "Github", 
+		// 		"summary": "Integer nec metus vitae nunc volutpat molestie nec vel odio. Cras venenatis tortor sed justo interdum venenatis. Integer nec metus vitae nunc volutpat molestie nec vel odio. Cras venenatis tortor sed justo interdum venenatis.",
+		// 		"url": "http://github.com/lusketeer" 
+		// 	},
+		// 	{	"heading": "Azurite", 
+		// 		"summary": "Integer nec metus vitae nunc volutpat molestie nec vel odio. Cras venenatis tortor sed justo interdum venenatis. Integer nec metus vitae nunc volutpat molestie nec vel odio. Cras venenatis tortor sed justo interdum venenatis.",
+		// 		"url": "#"
+		// 	},
+		// 	{	"heading": "Bloodstone", 
+		// 		"summary": "Integer nec metus vitae nunc volutpat molestie nec vel odio. Cras venenatis tortor sed justo interdum venenatis. Integer nec metus vitae nunc volutpat molestie nec vel odio. Cras venenatis tortor sed justo interdum venenatis.", 
+		// 		"url": "#"  
+		// 	},
+		// 	{ 	"heading": "Zircon", 
+		// 		"summary": "Integer nec metus vitae nunc volutpat molestie nec vel odio. Cras venenatis tortor sed justo interdum venenatis. Integer nec metus vitae nunc volutpat molestie nec vel odio. Cras venenatis tortor sed justo interdum venenatis.",
+		// 		"url": "#"
+		// 	},
+		// 	{ 	"heading": "Vircon", 
+		// 		"summary": "Integer nec metus vitae nunc volutpat molestie nec vel odio. Cras venenatis tortor sed justo interdum venenatis. Integer nec metus vitae nunc volutpat molestie nec vel odio. Cras venenatis tortor sed justo interdum venenatis.",
+		// 		"url": "#"
+		// 	}
+		// ];
 	}]);
 
 	app.directive("projectList", function(){
