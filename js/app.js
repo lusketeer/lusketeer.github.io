3cae8defa87851d76cc772b0677ca463ccbd426c
Luke <luke8520@gmail.com>
2014-06-27 15:36:24 -0700

    Changed initalized variables into app.js, removed images from index page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 (function() {
 	var app = angular.module('my-app', ["my-app-socials"]);
+
+	app.controller('myCtrl', function($scope) {
+		$scope.info = {
+			admin: {
+				name: {
+					first: "Luke",
+					last: "Lu",
+					nick: "Lusketeer"
+				}
+			}
+		}
+	});
 })();
